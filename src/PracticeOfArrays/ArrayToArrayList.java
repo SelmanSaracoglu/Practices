f20244657702fdbc7e03026a39d1c65e76a3a0c5
SelmanSaracoglu <selman.saracoglu@outlook.com>
2025-03-24 21:04:19 +0100

    updated

---
+++ src/PracticeOfArrays/ArrayToArrayList.java
@@ -0,0 +1,25 @@
+package PracticeOfArrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayToArrayList {
+
+    public static void main(String[] args) {
+
+        String[] array = { "Python", "JAVA", "PHP", "Perl", "C#", "C++" };
+
+        // Array'i ArrayList'e çevirme
+        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(array));
+        System.out.println("ArrayList:" + arrayList);
+
+        ArrayList<String> arrayList1 = new ArrayList<>();
+        for ( String element : array) {
+            arrayList1.add(element);
+        }
+        System.out.println("ArrayList: " + arrayList);
+
+
+
+    }
+}
