d9ac7c3a64406fbeb53e95053260d892fe476ad2
SelmanSaracoglu <selman.saracoglu@outlook.com>
2025-03-24 20:43:25 +0100

    updated

---
+++ src/PracticeOfArrays/FindIndex.java
@@ -0,0 +1,41 @@
+package PracticeOfArrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FindIndex {
+
+    public static void main(String[] args) {
+        int[] array = {12, 15, 43, 23, 56, 76, 78, 90, 77, 43};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Index'i bulmak istediginiz degeri giriniz: ");
+        int target = scanner.nextInt();
+        scanner.close();
+
+        int index =findIndex(array,target);
+
+        if (index != -1) {
+            System.out.println("Aranan değer " + target + ", dizide " + index + ". indekste bulunuyor.");
+        }else {
+            System.out.println("Aranan deger dizide bulunamadı.");
+        }
+
+
+        System.out.println("---------------------------------------");
+
+
+        // Primitif int[] dizisiyle Arrays.asList().indexOf() kullanamazsınız
+        Integer [] arrays = {12, 15, 43, 23, 56, 76, 78, 90, 77, 43};
+        int index2 = Arrays.asList(arrays).indexOf(43);
+        System.out.println("Aranan değerin indeksi: " + index2);
+    }
+
+    public static int findIndex(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target ) {
+                return i; // İlk bulunan index döndürülür
+            }
+        }
+        return -1; // Eğer bulunamazsa -1 döndürülür
+    }
+}
