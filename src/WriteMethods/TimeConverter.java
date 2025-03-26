7ed4f47d9103b04ec199306791d94c72e1b328cb
SelmanSaracoglu <selman.saracoglu@outlook.com>
2025-03-26 22:39:17 +0100

    updated

--- src/WriteMethods/TimeConverter.java
+++ src/WriteMethods/TimeConverter.java
@@ -4,20 +4,41 @@ import java.util.Scanner;
 
 public class TimeConverter {
 
-    public static int convertHoursToSeconds (int hours) {
-        return hours*3600;
-    }
+        public static int convertHoursToSeconds (int hours) {
+            return hours*3600;
+        }
+
+        public static int convertMinutesToSeconds (int minutes){
+            return minutes*60;
+        }
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
 
 
+            while (true) {
+                System.out.println("Saat mi dönüştürmek istiyorsunuz yoksa dakika mı?");
+                System.out.println("[S] Saat | [D] Dakika | [Q] Çıkış");
+                String input = scanner.nextLine();
 
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Lutfen donusturmek ıcın saat degeri giriniz: ");
-        int hours = scanner.nextInt();
-        int seconds = convertHoursToSeconds(hours);
-        System.out.println(hours + " saat = " + seconds + " saniyedir.");
-
-        scanner.close();
-    }
+                if (input.equalsIgnoreCase("q")){
+                    System.out.println("Cikis yapiliyor.");
+                    break;
+                }
+                if (input.equalsIgnoreCase("s")){
+                    System.out.println("Lutfen donusturmek ıcın saat degerini giriniz: ");
+                    int hours = scanner.nextInt();
+                    scanner.nextLine(); // Buffer temizleme
+                    int seconds = convertHoursToSeconds(hours);
+                    System.out.println(hours + " saat = " + seconds + " saniyedir.");
+                } else if (input.equalsIgnoreCase("D")) {
+                    System.out.println("Lutfen donusturmek ıcın dakika degerini giriniz: ");
+                    int minutes = scanner.nextInt();
+                    scanner.nextLine(); // Buffer temizleme
+                    int seconds = convertMinutesToSeconds(minutes);
+                    System.out.println(minutes + " dakika = " + seconds + " saniyedir.");
+                }
+            }
+            scanner.close();
+        }
 }
